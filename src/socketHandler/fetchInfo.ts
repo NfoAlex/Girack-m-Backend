9c3f3cca153b5044f902dd6904bbc58727ab5e66
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-17 05:39:06 +0900

    [fix] - Socketハンドラからのデータの返し方を完全に間違えていた
    睡眠大事

--- src/socketHandler/fetchInfo.ts
+++ src/socketHandler/fetchInfo.ts
@@ -6,25 +6,32 @@ module.exports = (io:Server) => {
   io.on("connection", (socket:Socket) => {
 
     //基本インスタンス情報(招待コードなど省く)を取得
-    socket.on("fetchServerInfoLimited", ():{
-      result: string,
-      data: IServerInfo
-    } => {
+    socket.on("fetchServerInfoLimited", () => {
+      /*
+      返し : {
+        result: "SUCCESS",
+        data: ServerInfoLimited<IServerInfo>
+      }
+      */
+
       //転送するインスタンス情報を削るためにクローンする
       const ServerInfoLimited:IServerInfo = structuredClone(ServerInfo);
       //招待コードを削除
       delete ServerInfoLimited.registration.invite.inviteCode;
 
       //返す
-      return { result:"SUCCESS", data:ServerInfoLimited }
+      socket.emit("ServerInfoLimited", { result:"SUCCESS", data:ServerInfoLimited });
     });
 
     //全てのインスタンス情報を取得
-    socket.on("fetchServerInfoFull", ():{
-      result: string,
-      data: IServerInfo
-    } => {
-      return { result:"SUCCESS", data:ServerInfo };
+    socket.on("fetchServerInfoFull", () => {
+      /*
+      返し : {
+        result: "SUCCESS"|"FAIL_MISSINGROLE",
+        data: ServerInfoLimited<IServerInfo>
+      }
+      */
+      socket.emit("ServerInfoFull", { result:"SUCCESS", data:ServerInfo });
     });
 
   });
