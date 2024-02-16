4b99922730e4c9f97d742611cba61aeb3ac234bd
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-17 04:26:32 +0900

    [add] - サーバー情報用type追加

---
+++ src/type/Server.ts
@@ -0,0 +1,24 @@
+export default interface ServerInfo {
+  servername: string,
+  registration: {
+    available: boolean,
+    invite: {
+      inviteOnly: boolean,
+      inviteCode: string,
+    }
+  },
+  config: {
+    PROFILE: {
+      iconMaxSize: number,
+      usernameMaxLength: number
+    },
+    CHANNEL: {
+      channelIdAnnounceRegistration: string,
+      defaultJoinOnRegister: string[],
+    },
+    MESSAGE: {
+      TxtMaxLength: number,
+      FileMaxSize: number
+    }
+  }
+};
