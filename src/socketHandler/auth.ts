8ec0bb2ef3416073f1ba818a03a425cc042b8dc8
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-19 04:52:35 +0900

    [change] - ログイン認証部分の結果用変数名変更

--- src/socketHandler/auth.ts
+++ src/socketHandler/auth.ts
@@ -18,17 +18,17 @@ module.exports = (io:Server) => {
 
       try {
         //認証処理
-        const authResult:{
+        const authData:{
           authResult:boolean,
           UserInfo: IUserInfo|null
         } = await authLogin(dat.username, dat.password);
 
-        console.log("auth :: authLogin : authResult->", authResult);
+        console.log("auth :: authLogin : authResult->", authData);
 
         //結果に応じて結果送信
-        if (authResult) {
+        if (authData.authResult) {
           //成功
-          socket.emit("RESULTauthLogin", {result:"SUCCESS", data:authResult.UserInfo});
+          socket.emit("RESULTauthLogin", {result:"SUCCESS", data:authData.UserInfo});
         } else {
           //認証無理だったら
           socket.emit("RESULTauthLogin", {result:"ERROR_WRONGINFO", data:null});
