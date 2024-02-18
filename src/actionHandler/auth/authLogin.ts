78099a9e1677a0c184f0d0d1a607914fa29068cd
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-19 04:37:49 +0900

    [add] - パスワード比較機能追加

--- src/actionHandler/auth/authLogin.ts
+++ src/actionHandler/auth/authLogin.ts
@@ -1,9 +1,10 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/USER.db");
+import fetchUser from "../../db/fetchUser";
 
 import { IUserInfo } from "../../type/User";
 
-export default function authLogin(username:string, password:string):boolean {
+export default async function authLogin(username:string, password:string):Promise<boolean> {
   //ユーザー検索、パスワードを比較する
   db.all("SELECT * FROM USERS_INFO WHERE name = ?", [username], (err:Error, datUser:IUserInfo) => {
     if (err) {
@@ -13,5 +14,17 @@ export default function authLogin(username:string, password:string):boolean {
     }
   });
 
-  return false;
+  //ユーザー情報取得
+  const RESULT = await fetchUser(null, username);
+
+  console.log("authLogin :: authLogin : RESULT ->", RESULT);
+
+  if (RESULT.length === 0) return false;
+
+  //パスワード比較、結果を返す
+  if (RESULT[0].password === password) {
+    return true;
+  } else {
+    return false;
+  }
 }
