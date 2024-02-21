2f6682fddee8d6aa6a1732b0de0d5d8ed750c21d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-02-21 18:01:31 +0900

    Regroup handlers (#6)
    * [change] - Socketハンドラを情報の種類ごとにわけることにした
    
    fetchInfo(情報取得) -> User / Server
    
    * [change] - ディレクトリ変更
    
    * [change] - アクションハンドラのディレクトリ移動に応じてインポート更新
    
    * [fix] - 忘れてた

--- src/socketHandler/Server.ts
+++ src/socketHandler/Server.ts
@@ -1,8 +1,5 @@
 import { Socket, Server } from "socket.io";
-import checkDataIntegrality from "../util/checkDataIntegrality";
 import { ServerInfo } from "../db/InitServer";
-import fetchUserConfig from "../actionHandler/fetchInfo/fetchUserConfig";
-import checkSession from "../actionHandler/auth/checkSession";
 
 import type IServerInfo from "../type/Server";
 import type IRequestSender from "../type/requestSender";
@@ -40,34 +37,6 @@ module.exports = (io:Server) => {
       /* ...ToDo :: 権限チェック */
 
       socket.emit("RESULTfetchServerInfoFull", { result:"SUCCESS", data:ServerInfo });
-    });
-
-    //ユーザーの設定データを取得
-    socket.on("fetchUserConfig", async (RequestSender:IRequestSender) => {
-      /*
-      返し : {
-        result: "SUCCESS"|"ERROR_DB_THING"|"ERROR_SESSION_ERROR",
-        data: ServerInfoLimited<IServerInfo>
-      }
-      */
-
-      /* TODO :: セッション認証 */
-      if (!(await checkSession(RequestSender.userId, RequestSender.sessionId))) {
-        socket.emit("RESULTfetchUserConfig", { result:"ERROR_SESSION_ERROR", data:null });
-        return;
-      }
-
-      try {
-        const configData = await fetchUserConfig(RequestSender.userId);
-        
-        console.log("fetchInfo :: socket(fetchUserConfig) : configData ->", configData);
-
-        //返す
-        socket.emit("RESULTfetchUserConfig", { result:"SUCCESS", data:configData });
-      } catch(e) {
-        //返す
-        socket.emit("RESULTfetchUserConfig", { result:"ERROR_DB_THING", data:null });
-      }
     });
 
   });
