5552534874eab189f0f73d90a3a4f0d3215f27ff
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-24 16:57:16 +0900

    [add] - ユーザー名検索に部分検索か完全一致検索か選べるように

--- src/db/searchUser.ts
+++ src/db/searchUser.ts
@@ -3,18 +3,34 @@ const db = new sqlite3.Database("./records/USER.db");
 
 import { IUserInfo } from "../type/User";
 
-export default async function searchUser(userName:string)
+export default async function searchUser(userName:string, rule:"FULL"|"PARTIAL")
 :Promise<IUserInfo[]> {
   return new Promise<IUserInfo[]>((resolve) => {
-    //ユーザー名でクエリが含まれるものを取得
-    db.all("SELECT * FROM USERS_INFO LIKE userName = %?%", [userName], (err:Error, datUser:IUserInfo[]) => {
-      if (err) {
-        console.log("searchUser :: ERROR ->", err);
-        resolve([]);
-      } else {
-        console.log("searchUser :: 検索結果->", userName, datUser);
-        resolve(datUser);
-      }
-    });
+    if (rule === "PARTIAL") {
+      //検索用クエリー
+      const searchQuery = "%" + userName + "%";
+      //ユーザー名でクエリが含まれるものを取得
+      db.all("SELECT * FROM USERS_INFO LIKE userName = ?", [searchQuery], (err:Error, datUser:IUserInfo[]) => {
+        if (err) {
+          console.log("searchUser :: ERROR ->", err);
+          resolve([]);
+        } else {
+          console.log("searchUser :: 検索結果->", userName, datUser);
+          resolve(datUser);
+        }
+      });
+    } else {
+      //検索用クエリー
+      //ユーザー名でクエリが含まれるものを取得
+      db.all("SELECT * FROM USERS_INFO WHERE userName = ?", [userName], (err:Error, datUser:IUserInfo[]) => {
+        if (err) {
+          console.log("searchUser :: ERROR ->", err);
+          resolve([]);
+        } else {
+          console.log("searchUser :: 検索結果->", userName, datUser);
+          resolve(datUser);
+        }
+      });
+    }
   });
 }
