6ae128e7f735f573874c4e903196c14e2eede870
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-02 14:01:57 +0900

    [add] - パスワード用テーブルを作成

--- src/db/InitUser.ts
+++ src/db/InitUser.ts
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/USER.db");
 
 db.serialize(() => {
-  //ユーザー基本情報を保存するUSER_INFOテーブルを無ければ作成
+  //ユーザー基本情報を保存するUSERS_INFOテーブルを無ければ作成
   db.run(`create table if not exists USERS_INFO(
     userId TEXT PRIMARY KEY,
     userName TEXT NOT NULL,
@@ -12,7 +12,13 @@ db.serialize(() => {
     banned BOOLEAN NOT NULL,
     password TEXT NOT NULL
   )`);
-  //ユーザーのセッション状態を保存するUSER_SESSIONテーブルを無ければ作成
+  //ユーザーのパスワードを保存するUSERS_PASSWORDテーブルを無ければ作成
+  db.run(`create table if not exists USERS_PASSWORD(
+    userId TEXT PRIMARY KEY,
+    password TEXT NOT NULL
+    FOREIGN KEY(userId) REFERENCES USERS_INFO(userId)
+  )`);
+  //ユーザーのセッション状態を保存するUSERS_SESSIONテーブルを無ければ作成
   db.run(`create table if not exists USERS_SESSION(
     sessionId TEXT PRIMARY KEY,
     userId TEXT NOT NULL,
@@ -21,7 +27,7 @@ db.serialize(() => {
     loggedinTimeFirst TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     FOREIGN KEY(userId) REFERENCES USERS_INFO(userId)
   )`);
-  //ユーザーの設定データを保存するUSER_CONFIGテーブルを無ければ作成
+  //ユーザーの設定データを保存するUSERS_CONFIGテーブルを無ければ作成
   db.run(`create table if not exists USERS_CONFIG(
     userId TEXT PRIMARY KEY,
     notification TEXT NOT NULL,
