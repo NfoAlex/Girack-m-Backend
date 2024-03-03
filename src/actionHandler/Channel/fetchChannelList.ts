2e1e48a36c8c283c28cb1cceda956d82a4e01548
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-03 17:54:37 +0900

    [fix] - チャンネルテーブル名のミス

--- src/actionHandler/Channel/fetchChannelList.ts
+++ src/actionHandler/Channel/fetchChannelList.ts
@@ -4,7 +4,7 @@ const db = new sqlite3.Database("./records/SERVER.db");
 export default async function fetchChannelList()
 :Promise<any[]|null> {
   return new Promise<any[]|null>((resolve) => {
-    db.all("SELECT * FROM CHANNEL", (err:Error, datChannels:any[]) => {
+    db.all("SELECT * FROM CHANNELS", (err:Error, datChannels:any[]) => {
       if (err) {
         console.log("fetchChannelList :: db : エラー->", err);
         resolve(null);
