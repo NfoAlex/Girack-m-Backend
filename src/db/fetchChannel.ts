9743138b9096a199c0860815fd9f4e0687d53cc0
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-03 17:18:43 +0900

    [remove] - 不要

--- src/db/fetchChannel.ts
+++ src/db/fetchChannel.ts
@@ -1,8 +1,6 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/SERVER.db");
 
-import { IUserInfo } from "../type/User";
-
 export default async function fetchChannel(channelId:string)
 :Promise<any[]|null> {
   return new Promise<any[]|null>((resolve) => {
