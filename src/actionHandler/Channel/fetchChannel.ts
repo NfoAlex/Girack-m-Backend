a7af31910da966933e4cd7a54b63d8e5e2933694
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-06 18:13:29 +0900

    [change] - チャンネル取得処理の返す値を変更

--- src/actionHandler/Channel/fetchChannel.ts
+++ src/actionHandler/Channel/fetchChannel.ts
@@ -3,14 +3,14 @@ const db = new sqlite3.Database("./records/SERVER.db");
 
 export default async function fetchChannel(channelId:string)
 :Promise<any[]|null> {
-  return new Promise<any[]|null>((resolve) => {
+  return new Promise<any|null>((resolve) => {
     db.all("SELECT * FROM CHANNELS WHERE channelId = ?", [channelId], (err:Error, datChannels:any[]) => {
       if (err) {
         console.log("fetchChannel :: db : エラー->", err);
         resolve(null);
       } else {
         console.log("fetchChannel :: db : 取得結果->", datChannels);
-        resolve(datChannels);
+        resolve(datChannels[0]);
       }
     });
   });
