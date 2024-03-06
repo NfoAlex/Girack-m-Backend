cb63876480d6a946079551c3eb682e5bc0b14990
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-06 18:24:47 +0900

    [change] - チャンネル情報取得処理を改善
    返り値をしっかりさせた

--- src/actionHandler/Channel/fetchChannel.ts
+++ src/actionHandler/Channel/fetchChannel.ts
@@ -1,16 +1,24 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/SERVER.db");
 
+import type { IChannelbeforeParsing, IChannel } from "../../type/Channel";
+
 export default async function fetchChannel(channelId:string)
-:Promise<any|null> {
-  return new Promise<any|null>((resolve) => {
-    db.all("SELECT * FROM CHANNELS WHERE channelId = ?", [channelId], (err:Error, datChannels:any[]) => {
+:Promise<IChannel|null> {
+  return new Promise<IChannel|null>((resolve) => {
+    db.all("SELECT * FROM CHANNELS WHERE channelId = ?", [channelId], (err:Error, datChannels:IChannelbeforeParsing[]) => {
       if (err) {
         console.log("fetchChannel :: db : エラー->", err);
         resolve(null);
       } else {
         console.log("fetchChannel :: db : 取得結果->", datChannels);
-        resolve(datChannels[0]);
+        //チャンネル情報を整形する
+        const infoGotIt:IChannel = {
+          ...datChannels[0],
+          isPrivate: datChannels[0].isPrivate==="1"?true:false,
+          speakableRole: datChannels[0].speakableRole.split(",")
+        };
+        resolve(infoGotIt);
       }
     });
   });
