bb1640c406cd74ff1a87031287b6247a15f616b7
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-07 20:55:09 +0900

    [add] - サーバー設定の更新処理を追加

---
+++ src/actionHandler/Server/updateServerConfig.ts
@@ -0,0 +1,23 @@
+import fs from "fs";
+import { ServerInfo } from "../../db/InitServer";
+
+import IServerInfo from "../../type/Server";
+
+export default function updateServerConfig(
+  ServerConfigUpdating:IServerInfo["config"]
+):boolean {
+  try {
+
+    //サーバー設定を更新
+    ServerInfo.config = ServerConfigUpdating;
+    //JSONファイルへ書き込み
+    fs.writeFileSync("./records/server.json", JSON.stringify(ServerInfo, null, 4));
+
+    return true;
+
+  } catch(e) {
+
+    return false;
+
+  }
+}
