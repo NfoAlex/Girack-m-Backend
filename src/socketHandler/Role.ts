ada0b11c031debd313684e0c8877897d89a10e22
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-09 03:57:28 +0900

    [add] - ロール作成処理の追加

--- src/socketHandler/Role.ts
+++ src/socketHandler/Role.ts
@@ -1,6 +1,9 @@
 import { Socket, Server } from "socket.io";
 import fetchRoles from "../actionHandler/Role/fetchRoles";
 import checkSession from "../actionHandler/auth/checkSession";
+import { roleCheck } from "../util/roleCheck";
+
+import IRequestSender from "../type/requestSender";
 
 module.exports = (io:Server) => {
   io.on("connection", (socket:Socket) => {
@@ -33,6 +36,42 @@ module.exports = (io:Server) => {
       } catch(e) {
         //返す
         socket.emit("RESULT::fetchRoles", { result:"ERROR_DB_THING", data:null });
+      }
+    });
+
+    //ロールを作成
+    socket.on("createRole", async (dat:{RequestSender:IRequestSender, roleData:any}) => {
+      /*
+      返し : {
+        result: "SUCCESS"|"ERROR_SESSION_ERROR"|"ERROR_DB_THING"|"ERROR_ROLE",
+        data: IUserRole[]|null
+      }
+      */
+
+      //セッション認証
+      if (!(await checkSession(dat.RequestSender))) {
+        socket.emit("RESULT::createRole", { result:"ERROR_SESSION_ERROR", data:null });
+        return;
+      }
+
+      try {
+        //ロール権限の確認
+        if (!(await roleCheck(dat.RequestSender.userId, "RoleManage"))) {
+          socket.emit("RESULT::createRole", { result:"ERROR_ROLE", data:null });
+          return;
+        }
+
+        //全ロールを取得
+        const roles = await fetchRoles();
+        //nullじゃないならそれを返す
+        if (roles !== null) {
+          socket.emit("RESULT::createRole", { result:"SUCCESS", data:roles });
+        } else {
+          socket.emit("RESULT::createRole", { result:"ERROR_DB_THING", data:null });
+        }
+      } catch(e) {
+        //返す
+        socket.emit("RESULT::createRole", { result:"ERROR_DB_THING", data:null });
       }
     });
 
