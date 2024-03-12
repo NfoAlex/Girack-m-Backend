0745f8843420ced0a496fe8a03ab57cd91ae291b
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-12 19:17:45 +0900

    [remove] - いらないじゃん

--- src/actionHandler/Server/updateServerConfig.ts
+++ src/actionHandler/Server/updateServerConfig.ts
@@ -1,18 +1,12 @@
 import fs from "fs";
 import { ServerInfo } from "../../db/InitServer";
-import roleCheck from "../../util/roleCheck";
 
 import IServerInfo from "../../type/Server";
 
 export default async function updateServerConfig(
-  userId: string,
   ServerConfigUpdating: IServerInfo["config"]
 ):Promise<boolean> {
   try {
-
-    //ロール確認
-    const roleCheckResult = await roleCheck(userId, "ServerManage");
-    if (!roleCheckResult) return false;
 
     //サーバー設定を更新
     ServerInfo.config = ServerConfigUpdating;
