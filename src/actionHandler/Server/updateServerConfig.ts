5da796580befdb409fd89b333b5e32836d93de12
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-12 22:12:57 +0900

    [add] - サーバー設定のテンプレにマージする形式で設定を更新するように

--- src/actionHandler/Server/updateServerConfig.ts
+++ src/actionHandler/Server/updateServerConfig.ts
@@ -1,7 +1,12 @@
 import fs from "fs";
 import { ServerInfo } from "../../db/InitServer";
+import mergeDeeply from "../../util/mergeDeeply";
 
 import IServerInfo from "../../type/Server";
+
+//サーバー設定テンプレ
+const ServerInfoTemplate:IServerInfo = JSON.parse(fs.readFileSync('./src/db/defaultValues/ServerInfo.json', 'utf-8'));
+
 
 export default function updateServerConfig(
   ServerConfigUpdating: IServerInfo["config"]
@@ -9,7 +14,7 @@ export default function updateServerConfig(
   try {
 
     //サーバー設定を更新
-    ServerInfo.config = ServerConfigUpdating;
+    ServerInfo.config = mergeDeeply(ServerInfoTemplate.config, ServerConfigUpdating);
     //JSONファイルへ書き込み
     fs.writeFileSync("./records/server.json", JSON.stringify(ServerInfo, null, 4));
 
