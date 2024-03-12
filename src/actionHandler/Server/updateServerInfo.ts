129c7bf19faf1298210076cfed30c4850ce18d89
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-03-13 06:35:09 +0900

    Manage instance information (#18)
    * [add] - サーバー設定更新時にロールチェックをするように
    
    * [remove] - いらないじゃん
    
    * [change] - awaitじゃなくてもいいね
    
    * [add] - インスタンス情報更新処理の追加
    
    * [add] - サーバー情報更新時にセッションを認証するように
    
    * [add/change] - 新規登録時に登録ができる設定であるかどうかを確認、美化
    
    * [add] - サーバー情報を更新したら全員に更新させるように
    
    * [add] - 招待コードを含めて設定を取得する際権限を確認するように
    
    * [add] - サーバー設定のテンプレにマージする形式で設定を更新するように
    
    * [add] - サーバー基本情報の更新もマージする感じでやるように

--- src/actionHandler/Server/updateServerInfo.ts
+++ src/actionHandler/Server/updateServerInfo.ts
@@ -2,18 +2,21 @@ import fs from "fs";
 import { ServerInfo } from "../../db/InitServer";
 import mergeDeeply from "../../util/mergeDeeply";
 
-import IServerInfo from "../../type/Server";
+import type IServerInfo from "../../type/Server";
 
 //サーバー設定テンプレ
 const ServerInfoTemplate:IServerInfo = JSON.parse(fs.readFileSync('./src/db/defaultValues/ServerInfo.json', 'utf-8'));
 
-export default function updateServerConfig(
-  ServerConfigUpdating: IServerInfo["config"]
+export default function updateServerInfo(
+  servernameNew:string,
+  registrationNew:IServerInfo["registration"]
 ):boolean {
   try {
 
-    //サーバー設定を更新
-    ServerInfo.config = mergeDeeply(ServerInfoTemplate.config, ServerConfigUpdating);
+    //データ更新
+    ServerInfo.servername = servernameNew;
+    ServerInfo.registration = mergeDeeply(ServerInfoTemplate.registration, registrationNew);
+
     //JSONファイルへ書き込み
     fs.writeFileSync("./records/server.json", JSON.stringify(ServerInfo, null, 4));
 
--- src/socketHandler/Server.ts
+++ src/socketHandler/Server.ts
@@ -3,6 +3,7 @@ import { ServerInfo } from "../db/InitServer";
 import checkSession from "../actionHandler/auth/checkSession";
 import updateServerConfig from "../actionHandler/Server/updateServerConfig";
 import roleCheck from "../util/roleCheck";
+import updateServerInfo from "../actionHandler/Server/updateServerInfo";
 
 import type IServerInfo from "../type/Server";
 import type IRequestSender from "../type/requestSender";
@@ -29,7 +30,7 @@ module.exports = (io:Server) => {
     });
 
     //全てのインスタンス情報を取得
-    socket.on("fetchServerInfoFull", (RequestSender:IRequestSender) => {
+    socket.on("fetchServerInfoFull", async (dat:{RequestSender:IRequestSender}) => {
       /*
       返し : {
         result: "SUCCESS"|"ERROR_MISSINGROLE",
@@ -37,7 +38,18 @@ module.exports = (io:Server) => {
       }
       */
 
-      /* ...ToDo :: 権限チェック */
+      /* セッション認証 */
+      if (!(await checkSession(dat.RequestSender))) {
+        socket.emit("RESULT::fetchServerInfoFull", { result:"ERROR_SESSION_ERROR", data:null });
+        return;
+      }
+
+      //権限確認
+      const roleCheckResult = await roleCheck(dat.RequestSender.userId, "ServerManage");
+      if (!roleCheckResult) {
+        socket.emit("RESULT::fetchServerInfoFull", { result:"ERROR_ROLE", data:null });
+        return;
+      }
 
       socket.emit("RESULT::fetchServerInfoFull", { result:"SUCCESS", data:ServerInfo });
     });
@@ -82,6 +94,49 @@ module.exports = (io:Server) => {
         }
       } catch(e) {
         socket.emit("RESULT::updateServerConfig", {result:"ERROR_DB_THING", data:null});
+      }
+    });
+
+    //インスタンスの基本情報の更新
+    socket.on("updateServerInfo", async (
+      dat:{
+        RequestSender: IRequestSender,
+        servername: IServerInfo["servername"]
+        registration: IServerInfo["registration"]
+      }
+    ) => {
+      /* セッション認証 */
+      if (!(await checkSession(dat.RequestSender))) {
+        socket.emit("RESULT::updateServerInfo", { result:"ERROR_SESSION_ERROR", data:null });
+        return;
+      }
+
+      try {
+        //権限確認
+        const roleCheckResult = await roleCheck(dat.RequestSender.userId, "ServerManage");
+        if (!roleCheckResult) {
+          socket.emit("RESULT::updateServerInfo", { result:"ERROR_ROLE", data:null });
+          return;
+        }
+
+        //更新処理
+        const updateServerInfoResult:boolean = updateServerInfo(dat.servername, dat.registration);
+        //もし成功なら現在のサーバー情報を返す
+        if (updateServerInfoResult) {
+          //転送するインスタンス情報を削るためにクローンする
+          const ServerInfoLimited:IServerInfo = structuredClone(ServerInfo);
+          //招待コードを削除
+          delete ServerInfoLimited.registration.invite.inviteCode;
+          
+          //全員にサーバー情報を返す
+          io.emit("RESULT::fetchServerInfoLimited", { result:"SUCCESS", data:ServerInfoLimited });
+          //サーバー情報編集者にすべてのサーバー情報を返す
+          socket.emit("RESULT::fetchServerInfoFull", { result:"SUCCESS", data:ServerInfo });
+        } else {
+          socket.emit("RESULT::updateServerInfo", { result:"ERROR_DB_THING", data:null });
+        }
+      } catch(e) {
+        socket.emit("RESULT::updateServerInfo", { result:"ERROR_DB_THING", data:null });
       }
     });
 
