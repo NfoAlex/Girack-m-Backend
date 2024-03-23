0a939222a3931f362977905c5f28245f6278ed8c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-24 03:45:26 +0900

    [add] - セッション情報のみでの認証を実装

--- src/socketHandler/auth.ts
+++ src/socketHandler/auth.ts
@@ -51,6 +51,15 @@ module.exports = (io:Server) => {
       }
     });
 
+    //セッションのみでの認証
+    socket.on("authSession", async (dat:{userId:string, sessionId:string}) => {
+      //セッション確認
+      if (!(await checkSession(dat))) {
+        socket.emit("RESULT::authSession", { result:"ERROR_SESSION_ERROR", data:null });
+        return;
+      }
+    });
+
     //新規登録
     socket.on("authRegister", async (dat:{username:string, inviteCode:string|null}) => {
       /*
