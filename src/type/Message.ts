2c6b85ef0e19f94db5f2b2d61724ccb5a6afe84b
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-03-31 13:52:09 +0900

    [add] - SQLからの生メッセージデータ用interface定義

--- src/type/Message.ts
+++ src/type/Message.ts
@@ -1,3 +1,10 @@
+export interface IMessageBeforeParsing {
+  channelId: string,
+  userId: string,
+  content: string,
+  reaction: string
+}
+
 export interface IMessage {
   channelId: string,
   userId: string,
