0ac8bbf62245fefec7df8fb8e64486925e5c4138
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-04 16:56:29 +0900

    [add] - メッセージ送信用ハンドラの土台作成

--- src/socketHandler/Message.ts
+++ src/socketHandler/Message.ts
@@ -1,17 +1,38 @@
 import { Socket, Server } from "socket.io";
+import IRequestSender from "../type/requestSender";
 import checkSession from "../actionHandler/auth/checkSession";
 
 module.exports = (io:Server) => {
   io.on("connection", (socket:Socket) => {
     
     //ログイン認証
-    socket.on("sendMessage", async (dat:{username:string, password:string}) => {
+    socket.on("sendMessage", async (
+      dat:{
+        RequestSender:IRequestSender,
+        message: {
+          channelId: string,
+          content: string
+        }
+      }
+    ) => {
       /*
       返し : {
         result: "SUCCESS"|"ERROR_MISSING_INFO"|"ERROR_SESSION_ERROR",
         data: IMessage|null
       }
       */
+
+      //セッション認証
+      if (!(await checkSession(dat.RequestSender))) {
+        socket.emit("RESULT::sendMessage", { result:"ERROR_SESSION_ERROR", data:null });
+        return;
+      }
+
+      try {
+
+      } catch(e) {
+
+      }
     });
 
   });
