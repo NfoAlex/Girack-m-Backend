63b6f5d866dca6f3c892a4676592af0da0d13c68
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-05-10 01:27:46 +0900

    [add] - チャンネルリスト取得時、プラベチャンネル用フィルターを適用

--- src/actionHandler/Channel/fetchChannelList.ts
+++ src/actionHandler/Channel/fetchChannelList.ts
@@ -1,6 +1,7 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/SERVER.db");
 import fetchUser from "../User/fetchUser";
+import roleCheck from "../../util/roleCheck";
 
 import type { IChannel } from "../../type/Channel";
 
@@ -11,13 +12,28 @@ export default async function fetchChannelList(userId: string)
   const userInfo = await fetchUser(userId, null);
   if (userInfo === null) return null;
 
+  //このユーザーはプラベチャンネルを見られるか調べる
+  const rolePowerViewPrivate = await roleCheck(userId, "ChannelViewPrivate");
+
   return new Promise<IChannel[]|null>((resolve) => {
     db.all("SELECT * FROM CHANNELS", (err:Error, datChannels:IChannel[]) => {
       if (err) {
         console.log("fetchChannelList :: db : エラー->", err);
         resolve(null);
       } else {
-        resolve(datChannels);
+        //プラベチャンネルを見れる権限があるなら参加確認しない
+        if (!rolePowerViewPrivate) {
+          //権限を持っておらず、参加していないなら除去
+          let datChannelsFiltered = datChannels.filter((channel) => {
+            return userInfo.channelJoined.includes(channel.channelId)
+                  ||
+                  !channel.isPrivate
+          });
+
+          resolve(datChannelsFiltered);
+        } else {
+          resolve(datChannels);
+        }
       }
     });
   });
