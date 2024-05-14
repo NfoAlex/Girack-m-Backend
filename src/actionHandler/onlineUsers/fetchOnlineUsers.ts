9e10e5534474517fc6603660ab8b52c1ddbc405e
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-14 15:34:50 +0900

    [add] - オンラインのユーザーを取得するSocketハンドラ作成

---
+++ src/actionHandler/onlineUsers/fetchOnlineUsers.ts
@@ -0,0 +1,34 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./records/ONLINEUSERS.db");
+
+export default async function fetchOnlineUsers(socketId: string|null, userId: string|null) {
+  try {
+
+    //オンラインのユーザーを取得
+    const onlineUsers = new Promise((resolve) => {
+      db.run(
+        `
+        SELECT userId from ONLINEUSERS
+        `,
+        (err:Error, onlineUsers:string[]) => {
+          if (err) {
+            resolve(null);
+            return;
+          } else {
+            resolve(onlineUsers);
+            return;
+          }
+        }
+      )
+    });
+
+    //返す
+    return onlineUsers;
+
+  } catch(e) {
+
+    console.log("fetchOnlineUsers :: エラー->", e);
+    return null;
+
+  }
+}
