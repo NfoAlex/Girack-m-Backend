e26a4ba4855d17e0baf02e1344b01e326e55973e
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-14 16:17:34 +0900

    [change] - ファイル名変えてオンラインのユーザーを削除する処理追加

---
+++ src/util/onlineUsers/removeUserOnlineBySocketId.ts
@@ -0,0 +1,25 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./records/ONLINEUSERS.db");
+
+export default async function removeUserOnlineBySocketId(socketId:string) {
+  try {
+
+    console.log("removeUserOnlineBySocketId :: socketId->", socketId);
+
+    //SocketIDに該当する接続を削除
+    db.run(
+      `
+      DELETE FROM ONLINE_USERS WHERE socketId=?
+      `,
+      socketId,
+    );
+
+    return;
+
+  } catch(e) {
+
+    console.log("removeUserOnlineBySocketId :: エラー->", e);
+    return;
+
+  }
+} 
