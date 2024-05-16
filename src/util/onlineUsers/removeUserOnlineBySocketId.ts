9cf3cac94ca022592862ac84eed41ec949975a9b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-16 11:03:38 +0900

    [add] - オンラインリストからユーザーIDを削除する際そのIDを返すように

--- src/util/onlineUsers/removeUserOnlineBySocketId.ts
+++ src/util/onlineUsers/removeUserOnlineBySocketId.ts
@@ -1,10 +1,47 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/ONLINEUSERS.db");
 
-export default async function removeUserOnlineBySocketId(socketId:string) {
+/**
+ * オンラインユーザーリストから一致するSocketIDを
+ * 持つユーザーIdを削除し、そのユーザーIdを返す
+ * @param socketId 
+ * @returns string
+ */
+export default async function removeUserOnlineBySocketId(socketId:string)
+:Promise<string | null> {
   try {
 
     console.log("removeUserOnlineBySocketId :: socketId->", socketId);
+
+    //切断するユーザーIdを取得
+    const userIdDisconnecting:string|null = await new Promise((resolve) => {
+      db.all(
+        `
+        SELECT userId FROM ONLINE_USERS WHERE socketId=?
+        `,
+        socketId,
+        (err:Error, onlineUsers:any[]) => {
+          if (err) {
+            console.log("removeUserOnlineBySocketId :: db : エラー->", err);
+            resolve(null);
+            return;
+          } else {
+            //配列の長さを確認して返す、空だったらnull
+            if (onlineUsers.length !== 0) {
+              resolve(onlineUsers[0]);
+            } else {
+              resolve(null);
+            }
+            return;
+          }
+        }
+      );
+    });
+
+    //もしユーザーIdがnullだったらそう返して停止
+    if (userIdDisconnecting === null) {
+      return null;
+    }
 
     //SocketIDに該当する接続を削除
     db.run(
@@ -20,12 +57,12 @@ export default async function removeUserOnlineBySocketId(socketId:string) {
       }
     );
 
-    return;
+    return userIdDisconnecting;
 
   } catch(e) {
 
     console.log("removeUserOnlineBySocketId :: エラー->", e);
-    return;
+    return null;
 
   }
 } 
