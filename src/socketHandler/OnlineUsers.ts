a3b911cb6e06db901d3b040a7c12afe3c61f73ac
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-16 13:13:32 +0900

    [add] - オンラインユーザー取得Socketハンドラにてセッション認証を追加

--- src/socketHandler/OnlineUsers.ts
+++ src/socketHandler/OnlineUsers.ts
@@ -4,18 +4,29 @@ import authLogin from "../actionHandler/auth/authLogin";
 import { IUserInfo } from "../type/User";
 import addUserOnline from "../util/onlineUsers/addUserOnline";
 import fetchOnlineUsers from "../actionHandler/onlineUsers/fetchOnlineUsers";
+import IRequestSender from "../type/requestSender";
+import checkSession from "../actionHandler/auth/checkSession";
 
 module.exports = (io:Server) => {
   io.on("connection", (socket:Socket) => {
     
     //オンラインユーザーを取得
-    socket.on("fetchOnlineUsers", async (dat:{username:string, password:string}) => {
+    socket.on("fetchOnlineUsers", async (dat:{RequestSender:IRequestSender}) => {
       /*
       返し : {
         result: "SUCCESS"|"ERROR_WRONGINFO",
-        data: 
+        data: userId[]
       }
       */
+
+      //セッション認証
+      if (!(await checkSession(dat.RequestSender))) {
+        socket.emit("RESULT::fetchOnlineUsers", {
+          result: "ERROR_WRONG_SESSION",
+          data: null
+        });
+        return;
+      }
 
       try {
         //認証処理
