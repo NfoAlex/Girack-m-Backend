7ebd19b02ca673da44bb355450cb1d595cba5539
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-05-23 11:50:34 +0900

    [change] - ユーザー検索用ハンドラを指定のチャンネル参加者限定で検索できるように
    あとＳＱＬ構文直した

--- src/actionHandler/User/searchUser.ts
+++ src/actionHandler/User/searchUser.ts
@@ -3,14 +3,44 @@ const db = new sqlite3.Database("./records/USER.db");
 
 import { IUserInfo } from "../../type/User";
 
-export default async function searchUser(userName:string, rule:"FULL"|"PARTIAL")
-:Promise<IUserInfo[]> {
+export default async function searchUser(
+  userName: string,
+  rule: "FULL"|"PARTIAL",
+  channelId?: string
+):Promise<IUserInfo[]> {
   return new Promise<IUserInfo[]>((resolve) => {
-    if (rule === "PARTIAL") { //部分検索
-      //検索用クエリー
-      const searchQuery = "%" + userName + "%";
-      //ユーザー名でクエリが含まれるものを取得
-      db.all("SELECT * FROM USERS_INFO LIKE userName = ?", [searchQuery], (err:Error, datUser:IUserInfo[]) => {
+    //チャンネルIdの指定があるかどうかでSQL文へ追加する文構成
+    const optionChannel =
+      channelId!==undefined 
+      ?
+        " AND channelJoined LIKE '%" + channelId + "%'"
+      :
+        "";
+
+  
+    //検索用クエリー
+    const searchQuery = 
+      rule==='PARTIAL'
+      ?
+        "%" + userName + "%" //部分検索
+      :
+        userName; //完全検索
+
+    console.log("searchUser :: sql文 ->", 
+      `
+      SELECT * FROM USERS_INFO
+        WHERE userName LIKE 
+      ` + searchQuery + optionChannel
+    );
+
+    //ユーザー名でクエリが含まれるものを取得
+    db.all(
+      `
+      SELECT * FROM USERS_INFO
+        WHERE userName LIKE ?
+      ` + optionChannel,
+      [searchQuery],
+      (err:Error, datUser:IUserInfo[]) => {
         if (err) {
           console.log("searchUser :: ERROR ->", err);
           resolve([]);
@@ -18,18 +48,8 @@ export default async function searchUser(userName:string, rule:"FULL"|"PARTIAL")
           //console.log("searchUser :: 検索結果->", userName, datUser);
           resolve(datUser);
         }
-      });
-    } else { //完全検索
-      //ユーザー名でクエリが含まれるものを取得
-      db.all("SELECT * FROM USERS_INFO WHERE userName = ?", [userName], (err:Error, datUser:IUserInfo[]) => {
-        if (err) {
-          console.log("searchUser :: ERROR ->", err);
-          resolve([]);
-        } else {
-          //console.log("searchUser :: 検索結果->", userName, datUser);
-          resolve(datUser);
-        }
-      });
-    }
+      }
+    );
+    
   });
 }
