e313b1dc3ab4d98c5e5379a96792d868f6338d64
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-06-01 03:20:11 +0900

    [add] - チャンネル情報取得関数にプラベ用にユーザーIdを参照するように

--- src/actionHandler/Channel/fetchChannel.ts
+++ src/actionHandler/Channel/fetchChannel.ts
@@ -1,10 +1,20 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/SERVER.db");
+import fetchUser from "../User/fetchUser";
+import roleCheck from "../../util/roleCheck";
 
 import type { IChannelbeforeParsing, IChannel } from "../../type/Channel";
 
-export default async function fetchChannel(channelId:string)
+export default async function fetchChannel(channelId:string, userId:string)
 :Promise<IChannel|null> {
+
+  //ユーザー情報を取得、ないならnull
+  const userInfo = await fetchUser(userId, null);
+  if (userInfo === null) return null;
+
+  //このユーザーがサーバー管理権限がありプラベを見られるか調べる
+  const roleServerManage = await roleCheck(userId, "ServerManage");
+
   return new Promise<IChannel|null>((resolve) => {
     db.all("SELECT * FROM CHANNELS WHERE channelId = ?", [channelId], (err:Error, datChannels:IChannelbeforeParsing[]) => {
       if (err) {
@@ -17,6 +27,13 @@ export default async function fetchChannel(channelId:string)
           resolve(null);
           return;
         } else {
+          //プラベで権限が無いならnull
+          if (datChannels[0].isPrivate && !roleServerManage) {
+            //返す
+            resolve(null);
+            return;
+          }
+
           //チャンネル情報を整形する
           const infoGotIt:IChannel = {
             ...datChannels[0],
