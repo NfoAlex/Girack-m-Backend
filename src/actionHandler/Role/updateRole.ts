5115392664ece85708c1b1e954218a34d4c1ec3e
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-06-01 03:53:14 +0900

    [fix] - チャンネル権限統一の忘れ

--- src/actionHandler/Role/updateRole.ts
+++ src/actionHandler/Role/updateRole.ts
@@ -33,9 +33,7 @@ export default async function updateRole(userId:string, roleData:IUserRole)
           color=?,
           ServerManage=?,
           RoleManage=?,
-          ChannelRename=?,
-          ChannelViewPrivate=?,
-          ChannelCreateAndDelete=?,
+          ChannelManage=?,
           UserManage=?,
           MessageDelete=?,
           MessageAttatchFile=?
