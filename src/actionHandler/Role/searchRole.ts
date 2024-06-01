dddfeab2bb1d1c855dfa71b56fe0928081d1c5d3
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-06-01 14:43:12 +0900

    [add] - ロール検索用の関数作成

---
+++ src/actionHandler/Role/searchRole.ts
@@ -0,0 +1,53 @@
+import sqlite3 from "sqlite3";
+import { IUserRole, IUserRoleBeforeParsing } from "../../type/User";
+const db = new sqlite3.Database("./records/USER.db");
+
+export default function searchRole(searchQuery:string, pageIndex:number) {
+  try {
+
+    return new Promise((resolve) => {
+      //ユーザー名でクエリが含まれるものを取得
+      db.all(
+        `
+        SELECT * FROM ROLES
+          WHERE roleName LIKE '%?%'
+          LIMIT 30
+        `,
+        [searchQuery],
+        (err:Error, datRole:IUserRoleBeforeParsing[]) => {
+          if (err) {
+            console.log("searchRole :: エラー ->", err);
+            resolve([]);
+          } else {
+            //変数パース用の配列変数
+            let roleDataParsed:IUserRole[] = [];
+            //変数のパース処理
+            for (let role of datRole) {
+              //配列プッシュ
+              roleDataParsed.push({
+                roleId: role.roleId,
+                name: role.name,
+                color: role.color,
+                ServerManage: role.ServerManage===1?true:false,
+                RoleManage: role.RoleManage===1?true:false,
+                ChannelManage: role.ChannelManage===1?true:false,
+                UserManage: role.UserManage===1?true:false,
+                MessageDelete: role.MessageDelete===1?true:false,
+                MessageAttatchFile: role.MessageAttatchFile===1?true:false
+              });
+            }
+            //パースしたものを返す
+            resolve(roleDataParsed);
+            return;
+          }
+        }
+      );
+    });
+
+  } catch(e) {
+
+    console.log("searchRole :: エラー->", e);
+    return null;
+
+  }
+}
