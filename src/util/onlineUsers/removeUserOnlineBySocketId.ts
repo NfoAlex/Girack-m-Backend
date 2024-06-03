b1d68ad963b81fcae76d1b62c2973ae4192a7e31
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-03 17:21:06 +0900

    Fix duplicated online users (#43)
    * [fix] - オンラインユーザー取得時同じユーザーを複数とらないように
    
    * [change] - indent
    
    * [add] - ユーザーが切断してもまだ他の接続があるとき切断通知をしないように

--- src/util/onlineUsers/removeUserOnlineBySocketId.ts
+++ src/util/onlineUsers/removeUserOnlineBySocketId.ts
@@ -1,4 +1,5 @@
 import sqlite3 from "sqlite3";
+import { IUserInfo } from "../../type/User";
 const db = new sqlite3.Database("./records/ONLINEUSERS.db");
 
 /**
@@ -20,7 +21,7 @@ export default async function removeUserOnlineBySocketId(socketId:string)
         SELECT userId FROM ONLINE_USERS WHERE socketId=?
         `,
         socketId,
-        (err:Error, onlineUsers:any[]) => {
+        (err:Error, onlineUsers:IUserInfo[]) => {
           if (err) {
             console.log("removeUserOnlineBySocketId :: db : エラー->", err);
             resolve(null);
@@ -28,7 +29,7 @@ export default async function removeUserOnlineBySocketId(socketId:string)
           } else {
             //配列の長さを確認して返す、空だったらnull
             if (onlineUsers.length !== 0) {
-              resolve(onlineUsers[0]);
+              resolve(onlineUsers[0].userId);
             } else {
               resolve(null);
             }
@@ -43,21 +44,48 @@ export default async function removeUserOnlineBySocketId(socketId:string)
       return null;
     }
 
-    //SocketIDに該当する接続を削除
-    db.run(
-      `
-      DELETE FROM ONLINE_USERS WHERE socketId=?
-      `,
-      socketId,
-      (err:Error, onlineUsers:any[]) => {
-        if (err) {
-          console.log("removeUserOnlineBySocketId :: db : エラー->, err");
-          return;
-        }
-      }
-    );
+    //切断処理とそのユーザーがまだいるかどうか判断処理
+    return new Promise((resolve) => {
+      db.serialize(() => {
+        //SocketIDに該当する接続を削除
+        db.run(
+          `
+          DELETE FROM ONLINE_USERS WHERE socketId=?
+          `,
+          socketId,
+          (err:Error, onlineUsers:any[]) => {
+            if (err) {
+              console.log("removeUserOnlineBySocketId :: db(DELETE socketId) : エラー->, err");
+              return;
+            }
+          }
+        );
 
-    return userIdDisconnecting;
+        //切断するユーザーIdのSocket数を調べて0なら切断したと送信
+        db.all(
+          `
+          SELECT COUNT(*) FROM ONLINE_USERS WHERE userId=?
+          `,
+          userIdDisconnecting,
+          (err:Error, onlineUsersNum:{"COUNT(*)":number}[]) => {
+            if (err) {
+              console.log("removeUserOnlineBySocketId :: db(COUNT(*)) : エラー->", err);
+            } else {
+              console.log("removeUserOnlineBySocketId :: db(COUNT(*)) : カウント->", onlineUsersNum);
+              //ここで接続数調べ
+              if (onlineUsersNum[0]['COUNT(*)'] === 0) {
+                resolve(userIdDisconnecting);
+                return;
+              } else {
+                resolve(null);
+                return;
+              }
+            }
+          }
+        );
+
+      });
+    });
 
   } catch(e) {
 
