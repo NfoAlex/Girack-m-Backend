f1f3bdf3e8c72250c44a9eeee971b711de1935fc
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-04 10:44:42 +0900

    [add] - linkDataを追加するmigrationスクリプト作成

---
+++ src/db/migration/Message/20240603.ts
@@ -0,0 +1,24 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./records/MESSAGE.db");
+
+/**
+ * すべてのチャンネル履歴テーブルへlinkDataカラムを追加
+ */
+export default async function migration20240603() {
+  //チャンネル分のテーブルへlinkDataカラムを追加
+  return new Promise ((resolve)=> {
+      db.all(
+        `
+        SELECT name FROM sqlite_master WHERE type='table';
+        `,
+        (err:Error, tables:[{name:string}]) => {
+          console.log("20240603 :: tables->", tables);
+          //テーブルを追加
+          for (let channelName of tables) {
+            db.run(`ALTER TABLE ` + channelName.name + ` ADD linkData BLOB`)
+          }
+          return;
+        }
+      );
+  });
+}
