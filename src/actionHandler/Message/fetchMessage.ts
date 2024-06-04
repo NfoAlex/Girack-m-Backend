abe36b5b13c1d2261f5d3074f248bc069a933b16
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-04 14:40:45 +0900

    [add] - メッセージ単体取得用のパース処理にリンクプレビューデータ分も追加

--- src/actionHandler/Message/fetchMessage.ts
+++ src/actionHandler/Message/fetchMessage.ts
@@ -31,6 +31,7 @@ export default function fetchMessage(
             //生メッセージデータを扱える形にパースする
             const messageParsed:IMessage = {
               ...message[0],
+              linkData: JSON.parse(message[0].linkData),
               reaction: JSON.parse(message[0].reaction)
             };
             resolve(messageParsed);
