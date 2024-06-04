0f5a040086831719cd01d832801e2b2b4619e2cd
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-04 15:30:21 +0900

    [add] - WIP リンクプレビュー生成関数の土台作成

---
+++ src/util/genLinkPreview.ts
@@ -0,0 +1,25 @@
+import sqlite3 from "sqlite3";
+import { IMessage } from "../type/Message";
+const db = new sqlite3.Database("./records/MESSAGE.db");
+
+export default async function genLinkPreview(message:IMessage) {
+  try {
+    
+    const ogs = require('open-graph-scraper');
+    const options = { url: 'http://ogp.me/' };
+    ogs(options)
+    .then((data:any) => {
+      const { error, html, result, response } = data;
+      console.log('error:', error);  // This returns true or false. True if there was an error. The error itself is inside the result object.
+      console.log('html:', html); // This contains the HTML of page
+      console.log('result:', result); // This contains all of the Open Graph results
+      console.log('response:', response); // This contains response from the Fetch API
+    });
+
+  } catch(e) {
+
+    console.log("getLinkPreview :: エラー->", e);
+    return null;
+
+  }
+}
