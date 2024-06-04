6cd5a0c251dc8086f67f083381ac76f3abbdb68c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-04 10:54:25 +0900

    [fix] - 重複したカラムへの対応忘れてた

--- src/db/migration/Message/20240603.ts
+++ src/db/migration/Message/20240603.ts
@@ -7,18 +7,19 @@ const db = new sqlite3.Database("./records/MESSAGE.db");
 export default async function migration20240603() {
   //チャンネル分のテーブルへlinkDataカラムを追加
   return new Promise ((resolve)=> {
-      db.all(
-        `
-        SELECT name FROM sqlite_master WHERE type='table';
-        `,
-        (err:Error, tables:[{name:string}]) => {
-          console.log("20240603 :: tables->", tables);
-          //テーブルを追加
-          for (let channelName of tables) {
-            db.run(`ALTER TABLE ` + channelName.name + ` ADD linkData BLOB`)
-          }
-          return;
+    db.all(
+      `
+      SELECT name FROM sqlite_master WHERE type='table';
+      `,
+      (err:Error, tables:[{name:string}]) => {
+        //console.log("20240603 :: tables->", tables);
+        
+        //ループしてlinkDataカラムを追加
+        for (let channelName of tables) {
+          db.run(`ALTER TABLE ` + channelName.name + ` ADD linkData BLOB`, (err:Error)=>{});
         }
-      );
+        return;
+      }
+    );
   });
 }
