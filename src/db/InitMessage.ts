88d96992d1928f340ae56109cbb6f6fdc0fd8d91
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-04 16:02:37 +0900

    [add] - これも

--- src/db/InitMessage.ts
+++ src/db/InitMessage.ts
@@ -14,7 +14,7 @@ db.serialize(() => {
     channelId TEXT NOT NULL,
     userId TEXT NOT NULL,
     content TEXT NOT NULL,
-    linkData TEXT,
+    linkData TEXT DEFAULT '{}',
     time TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     reaction TEXT NOT NULL
   )`);
