ca4d0351ead7e06c7c258d66f32301951ff804ab
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-07 13:51:08 +0900

    [change] - 画像用リンクならリンクプレビュー処理をしないように

--- src/util/genLinkPreview.ts
+++ src/util/genLinkPreview.ts
@@ -31,30 +31,44 @@ export default async function genLinkPreview(
       "0":{}
     };
 
-    //プレビューデータ化処理
-    await ogs(options)
-    .then((data:any) => {
-      const { error, html, result, response } = data;
-      //console.log('error:', error);  // This returns true or false. True if there was an error. The error itself is inside the result object.
-      //console.log('html:', html); // This contains the HTML of page
-      console.log('result:', result); // This contains all of the Open Graph results
-      //パース
-      if (result.ogType === "website" || result.ogType === "article") {
-        previewResult = {
-          "0": {
-            contentType: "text/html",
-            mediaType: result.ogType,
-            url: result.ogUrl,
-            siteName: result.ogSiteName,
-            title: result.ogTitle,
-            description: result.ogDescription,
-            images: result.ogImage,
-            favicon: result.favicon
-          }
-        };
-      }
-      //console.log('response:', response); // This contains response from the Fetch API
-    });
+    console.log("genLinkPreview :: match as image?->", urls[0].match(/(https?:\/\/.*\.(?:png|jpg))/g));
+
+    //もしURLが画像用ならここで処理して終了
+    if (urls[0].match(/(https?:\/\/.*\.(?:png|jpg))/g) !== null) {
+      previewResult = {
+        "0": {
+          mediaType: "image",
+          url: urls[0],
+        }
+      };
+    } else {
+
+      //プレビューデータ化処理
+      await ogs(options)
+      .then((data:any) => {
+        const { error, html, result, response } = data;
+        //console.log('error:', error);  // This returns true or false. True if there was an error. The error itself is inside the result object.
+        //console.log('html:', html); // This contains the HTML of page
+        console.log('result:', result); // This contains all of the Open Graph results
+        //パース
+        if (result.ogType === "website" || result.ogType === "article") {
+          previewResult = {
+            "0": {
+              contentType: "text/html",
+              mediaType: result.ogType,
+              url: result.ogUrl,
+              siteName: result.ogSiteName,
+              title: result.ogTitle,
+              description: result.ogDescription,
+              images: result.ogImage,
+              favicon: result.favicon
+            }
+          };
+        }
+        console.log('response:', response); // This contains response from the Fetch API
+      });
+
+    }
 
     //プレビューデータの書き込み処理
     return new Promise((resolve)=> {
