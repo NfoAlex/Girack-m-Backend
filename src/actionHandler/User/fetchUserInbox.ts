dce836b2f9106bb5a4aabc0e8ab6383b45abec05
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-12 04:01:56 +0900

    [add] - Inboxを取得する関数作成

---
+++ src/actionHandler/User/fetchUserInbox.ts
@@ -0,0 +1,30 @@
+import sqlite3 from "sqlite3";
+import { IUserInbox } from "../../type/User";
+const db = new sqlite3.Database("./records/USER.db");
+
+export default async function fetchUserInbox(userId: string):Promise<IUserInbox|null> {
+  try {
+
+    return new Promise((resolve) => {
+      db.all(
+        `
+        SELECT inbox FROM USERS_SAVES
+          WHERE userId=?
+        `,
+        userId,
+        (err:Error, inboxData:[{inbox: string}]) => {
+          //console.log("fetchUserChannelOrder :: db : channelOrder->", channelOrderData);
+          //文字列をJSONにしてから返す
+          resolve(JSON.parse(inboxData[0].inbox));
+          return;
+        }
+      )
+    });
+
+  } catch(e) {
+
+    console.log("fetchInbox :: エラー->", e);
+    return null;
+
+  }
+}
