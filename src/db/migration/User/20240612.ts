b504b022f9a4e4eaf2a35b8becb87178330564bd
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-12 03:23:56 +0900

    [add] - inbox用のカラムを追加、マイグレーションも用意

---
+++ src/db/migration/User/20240612.ts
@@ -0,0 +1,20 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./records/USER.db");
+
+/**
+ * USERS_SAVEへinboxカラム追加
+ */
+export default async function migration20240612() {
+  db.all(
+    `
+    ALTER TABLE USERS_SAVES ADD inbox TEXT DEFAULT '{}';
+    `,
+    (err:Error, tables:[{name:string}]) => {
+      if (err) {
+        console.log("migration(20240612) :: エラー!->", err);
+        throw err;
+      }
+      return;
+    }
+  );
+}
