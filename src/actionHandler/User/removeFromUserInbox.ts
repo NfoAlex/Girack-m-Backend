545769164ca2892bb4736adadde52bbb30f7da75
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-15 17:24:25 +0900

    [change] - まずInboxの通知削除関数を元に

--- src/actionHandler/User/removeFromUserInbox.ts
+++ src/actionHandler/User/removeFromUserInbox.ts
@@ -10,66 +10,41 @@ export default async function removeFromUserInbox(
 ):Promise<boolean> {
   try {
 
-    let flagDone:boolean = false;
-    let flagDoneConfirm:boolean = false;
+    //このユーザーのInbox取得
+    const inboxEditing = await fetchUserInbox(userId);
+    if (!inboxEditing) {
+      return false;
+    };
+
+    //指定の項目Idの場所取得
+    const indexOfItemId = inboxEditing[inboxCategory][channelId].indexOf(itemId);
+      //消したい項目Idが無ければエラーとして返す
+    if (indexOfItemId === -1) {
+      return false;
+    }
+
+    //指定のIdの項目を削除
+    inboxEditing[inboxCategory][channelId].splice(indexOfItemId, 1);
 
     return new Promise((resolve) => {
-      const INTERVALremoveFromUserInbox = setInterval(async () => {
-
-        //このユーザーのInbox取得
-        const inboxEditing = await fetchUserInbox(userId);
-        if (!inboxEditing) {
-          resolve(false);
-          return;
-        };
-
-        //指定の項目Idの場所取得
-        const indexOfItemId = inboxEditing[inboxCategory][channelId].indexOf(itemId);
-          //消したい項目Idが無ければエラーとして返す
-        if (indexOfItemId === -1) {
-          resolve(false);
-          return;
+      db.run(
+        `
+        UPDATE USERS_SAVES SET inbox=?
+          WHERE userId=?
+        `,
+        [JSON.stringify(inboxEditing), userId],
+        (err:Error, inboxData:[{inbox: string}]) => {
+          if (err) {
+            console.log("removeFromUserInbox :: エラー->", err);
+            resolve(false);
+            return;
+          } else {
+            //console.log("removeFromUserInbox :: db : channelOrder->", channelOrderData);
+            resolve(true);
+            return;
+          }
         }
-
-        //指定のIdの項目を削除
-        inboxEditing[inboxCategory][channelId].splice(indexOfItemId, 1);
-
-        flagDone = await new Promise((resolve) => {
-          db.run(
-            `
-            UPDATE USERS_SAVES SET inbox=?
-              WHERE userId=?
-            `,
-            [JSON.stringify(inboxEditing), userId],
-            (err:Error, inboxData:[{inbox: string}]) => {
-              if (err) {
-                console.log("removeFromUserInbox :: エラー->", err);
-                resolve(false);
-                return;
-              } else {
-                //console.log("removeFromUserInbox :: db : channelOrder->", channelOrderData);
-                resolve(true);
-                return;
-              }
-            }
-          )
-        });
-
-        if (flagDoneConfirm) {
-          console.log("removeFromuserInbox :: INTERVAL : ループ終了");
-          clearInterval(INTERVALremoveFromUserInbox);
-          resolve(true);
-          return;
-        }
-
-        if (flagDone) {
-          flagDoneConfirm = true;
-        }
-
-        console.log("removeFromuserInbox :: INTERVAL : ループ");
-
-      }, 500);
-
+      )
     });
 
   } catch(e) {
