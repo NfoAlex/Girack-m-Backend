120cad009396c1ba4605ab8563c8ad90c8ca6a17
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-28 17:31:59 +0900

    [add] - 返信データ用のmigration作成

---
+++ src/db/migration/Message/20240624.ts
@@ -0,0 +1,23 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./records/MESSAGE.db");
+
+/**
+ * すべてのチャンネル履歴テーブルへreplyDataカラムを追加
+ */
+export default async function migrationMessage20240624() {
+  //チャンネル分のテーブルへreplyDataカラムを追加
+  db.all(
+    `
+    SELECT name FROM sqlite_master WHERE type='table';
+    `,
+    (err:Error, tables:[{name:string}]) => {
+      //console.log("20240624 :: tables->", tables);
+
+      //ループしてlinkDataカラムを追加
+      for (let channelName of tables) {
+        db.run(`ALTER TABLE ` + channelName.name + ` ADD replyData TEXT DEFAULT '{}'`, (err:Error)=>{});
+      }
+      return;
+    }
+  );
+}
