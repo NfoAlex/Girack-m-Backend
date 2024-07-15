ff04c37c6bd737916a62abd3204f04af9d5e6432
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-07-15 22:04:37 +0900

    [add] - ファイルアップロード時にファイルインデックスへ書き込むように

--- src/fileHandler/File/uploadfile.ts
+++ src/fileHandler/File/uploadfile.ts
@@ -1,3 +1,8 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./records/FILEINDEX.db");
+
+import type IRequestSender from "../../type/requestSender";
+
 /**
  * ファイルのアップロード処理
  * @param req 
@@ -7,10 +12,56 @@
 export default async function uploadfile(req:any, res:any) {
   try {
 
+    console.log("/uploadfile :: ファイルが書き込まれました");
     // 補足データ（metadata）を取得し、JSONとしてパース
     console.log("/uploadfile :: これからの処理に使うreq.body.metadata->", req.body.metadata);
 
-    console.log("/uploadfile :: ファイルが書き込まれました");
+    //送信者情報取り出し
+    const RequestSender:IRequestSender = JSON.parse(req.body.metadata);
+
+    db.serialize(() => {
+
+      //この人用のファイルインデックス用テーブル作成
+      db.run(
+        `
+        create table if not exists FILE` + RequestSender.userId + `(
+          id TEXT PRIMARY KEY,
+          userId TEXT DEFAULT ` + RequestSender.userId + `,
+          name TEXT NOT NULL,
+          isPublic BOOLEAN NOT NULL DEFAULT 0,
+          size NUMBER NOT NULL
+        )
+        `
+      );
+
+      //ファイルId生成
+      const fileIdGenerated = () => {
+        let id = "";
+        for (let i=0; i<10; i++) {
+          id += Math.floor(Math.random()*9).toString();
+        }
+        return RequestSender.userId + id;
+      }
+
+      //ファイルデータ書き込み
+      db.run(
+        `
+        INSERT INTO FILE` + RequestSender.userId + ` (
+          id, name, size
+        )
+        VALUES (?, ?, ?)
+        `,
+        [fileIdGenerated(), req.file.originalname, req.file.size],
+        (err:Error) => {
+          if (err) {
+            console.log("uploadfile :: エラー->", err);
+            res.status(500).send("ERROR_DB_THING");
+            return;
+          }
+        }
+      )
+
+    });
 
     res.status(200).send("SUCCESS");
 
