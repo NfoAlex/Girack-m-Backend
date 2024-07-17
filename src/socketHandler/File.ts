c6a2aaed6b4fc85fc635f215160b932d25e24881
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-07-17 18:54:41 +0900

    [add] - フォルダーを取得と作成をするSocketハンドラ作成

--- src/socketHandler/File.ts
+++ src/socketHandler/File.ts
@@ -6,6 +6,8 @@ import type IRequestSender from "../type/requestSender";
 import fetchFileIndex from "../actionHandler/File/fetchFileIndex";
 import deleteFile from "../actionHandler/File/deleteFile";
 import fetchFileInfo from "../util/FIle/fetchFileInfo";
+import createFolder from "../actionHandler/File/createFolder";
+import fetchFolders from "../util/FIle/fetchFolders";
 
 module.exports = (io:Server) => {
   io.on("connection", (socket:Socket) => {
@@ -120,6 +122,68 @@ module.exports = (io:Server) => {
           result: "ERROR_INTERNAL_THING",
           data: null
         });
+      }
+    });
+
+    //フォルダーを取得
+    socket.on("fetchFolders", async (dat:{RequestSender:IRequestSender}) => {
+      //セッション認証
+      if (!(await checkSession(dat.RequestSender))) {
+        socket.emit("RESULT::fetchFolders", {
+          result: "ERROR_WRONG_SESSION",
+          data: null
+        });
+        return;
+      }
+
+      try {
+        //フォルダーを取得
+        const folder = await fetchFolders(dat.RequestSender.userId);
+        //取得内容に応じて結果を送信
+        if (folder !== null) {
+          socket.emit("RESULT::fetchFolders", { result:"SUCCESS", dat:folder });
+        } else {
+          socket.emit("RESULT::fetchFolders", { result:"ERROR_DB_THING", dat:null });
+        }
+      } catch(e) {
+        console.log("socket(fetchFolders) :: エラー->", e);
+        socket.emit("RESULT::fetchFolders", { result:"ERROR_INTERNAL_THING", dat:null });
+      }
+    });
+
+    //フォルダーを作成する
+    socket.on("createFolder", async (
+      dat: {
+        RequestSender: IRequestSender,
+        folderName: string,
+        directory: string
+      }
+    ) => {
+      //セッション認証
+      if (!(await checkSession(dat.RequestSender))) {
+        socket.emit("RESULT::createFolder", {
+          result: "ERROR_WRONG_SESSION",
+          data: null
+        });
+        return;
+      }
+
+      try {
+        //フォルダーを作成する
+        const createFolderResult = await createFolder(
+          dat.RequestSender.userId,
+          dat.folderName,
+          dat.directory
+        );
+        //結果に応じてそう送信
+        if (createFolderResult) {
+          socket.emit("RESULT::createFolder", { result:"SUCCESS", dat:null });
+        } else {
+          socket.emit("RESULT::createFolder", { result:"ERROR_DB_THING", dat:null });
+        }
+      } catch(e) {
+        console.log("socket(createFolder) :: エラー->", e);
+        socket.emit("RESULT::createFolder", { result:"ERROR_INTERNAL_THING", dat:null });
       }
     });
 
