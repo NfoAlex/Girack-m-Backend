264151e14fb1afc69fa0bfd63d4fb696c0c68fa2
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-07-19 02:04:29 +0900

    [add] - フォルダ情報を単体で取得する関数作成

---
+++ src/fileHandler/File/fetchFolderInfo.ts
@@ -0,0 +1,36 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("./records/FILEINDEX.db");
+
+import { IFolder } from "../../type/File";
+
+export default async function fetchFolderInfo(userId:string, folderId:string):Promise<IFolder|null> {
+  try {
+
+    return new Promise((resolve) => {
+      db.all(
+        `
+        SELECT * FROM FOLDERS
+          WHERE userId=? AND id=?
+        `,
+        [userId, folderId],
+        (err:Error, folderInfo:[IFolder]) => {
+          if (err) {
+            console.log("fetchFolderInfo :: db : エラー->", err);
+            resolve(null);
+            return;
+          } else {
+            console.log("fetchFolderInfo :: db : 結果->", folderInfo);
+            resolve(folderInfo[0]);
+            return;
+          }
+        }
+      );
+    });
+
+  } catch(e) {
+
+    console.log("folderInfo :: エラー->", e);
+    return null;
+
+  }
+}
