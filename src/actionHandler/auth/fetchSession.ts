cbb09dbb43ea745e86558c69debc473abacdf7a5
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-12 05:06:12 +0900

    [remove] - また忘れてた

--- src/actionHandler/auth/fetchSession.ts
+++ src/actionHandler/auth/fetchSession.ts
@@ -1,9 +1,6 @@
-import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./records/USER.db");
-
 import Database from 'better-sqlite3';
-const _db = new Database('./records/USER.db', {verbose: console.log });
-_db.pragma('journal_mode = WAL');
+const db = new Database('./records/USER.db', {verbose: console.log });
+db.pragma('journal_mode = WAL');
 
 import type { IUserSession } from "../../type/User";
 
@@ -20,7 +17,7 @@ export default function fetchSession(_userId: string, _indexNumber: number)
     //オフセットでずらすデータ数
     const offsetNum = 10 * (_indexNumber-1 || 0);
 
-    const sessionData = _db.prepare(
+    const sessionData = db.prepare(
       "SELECT * FROM USERS_SESSION WHERE userId=? LIMIT 10 OFFSET ?"
     ).all(_userId, offsetNum) as IUserSession[];
 
