33a627d44854df6d95a5af38577274ded7e09a35
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-12 03:37:14 +0900

    [change] - fetchUserをbetter-sqlite3へ

--- src/actionHandler/User/fetchUser.ts
+++ src/actionHandler/User/fetchUser.ts
@@ -1,70 +1,39 @@
-import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./records/USER.db");
+import Database from 'better-sqlite3';
+const _db = new Database('./records/USER.db', {verbose: console.log });
+_db.pragma('journal_mode = WAL');
 
-import { IUserInfo, IUserInfoBeforeParsing } from "../../type/User";
+import type { IUserInfo } from "../../type/User";
 
-export default async function fetchUser(userId:string|null, username:string|null)
+export default async function fetchUser(_userId:string|null, _username:string|null)
 :Promise<IUserInfo|null> {
   return new Promise<IUserInfo|null>((resolve) => {
-    //ユーザーIDが引数に無かったらIDで検索する
-    if (userId === null) {
-      //ユーザーを名前で検索
-      db.all("SELECT * FROM USERS_INFO WHERE userName = ?", [username], (err:Error, datUser:IUserInfoBeforeParsing[]) => {
-        if (err) {
-          console.log("fetchUser(userName) :: ERROR ->", err);
-          resolve(null);
-        } else {
-          //console.log("fetchUser(userName) :: 検索結果->", username, datUser);
-          //そもそも結果が無いならそう返す
-          if (datUser.length === 0) {
-            console.log("fetchUser(userName) :: ERROR -> データが空");
-            resolve(null);
-            return;
-          }
+    //ユーザーIDが引数に無かったらユーザー名で検索する
+    if (_userId === null) {
+      const userInfo = _db.prepare(
+        "SELECT * FROM USERS_INFO WHERE userName = ?"
+      ).get(_username) as IUserInfo|undefined;
+      console.log("fetchUser :: 結果->", userInfo);
 
-          //console.log("fetchUser(userName) :: データ長->", datUser.length);
+      if (userInfo !== undefined) {
+        resolve(userInfo);
+        return;
+      }
 
-          //ユーザー情報を取得して利用できるに整備
-          let datUserParsed:IUserInfo = {
-            userId: datUser[0].userId,
-            userName: datUser[0].userName,
-            role: datUser[0].role.split(","),
-            channelJoined: datUser[0].channelJoined.split(","),
-            banned: datUser[0].banned===1?true:false
-          };
-
-          //ユーザー情報を返す
-          resolve(datUserParsed);
-        }
-      });
-    } else {
-      //ユーザーをIDで検索
-      db.all("SELECT * FROM USERS_INFO WHERE userId = ?", [userId], (err:Error, datUser:IUserInfoBeforeParsing[]) => {
-        if (err) {
-          console.log("fetchUser(userId) :: ERROR ->", err);
-          resolve(null);
-        } else {
-          //console.log("fetchUser(userId) :: 検索結果->", userId, datUser);
-          //そもそも結果が無いならそう返す
-          if (datUser.length === 0) {
-            console.log("fetchUser(userId) :: ERROR -> データが空");
-            resolve(null);
-            return;
-          }
-
-          //ユーザー情報を取得して利用できるに整備
-          let datUserParsed:IUserInfo = {
-            userId: datUser[0].userId,
-            userName: datUser[0].userName,
-            role: datUser[0].role.split(","),
-            channelJoined: datUser[0].channelJoined.split(","),
-            banned: datUser[0].banned===1?true:false
-          };
-
-          //ユーザー情報を返す
-          resolve(datUserParsed);
-        }
-      });
+      resolve(null);
+      return;
     }
+
+    const userInfo = _db.prepare(
+      "SELECT * FROM USERS_INFO WHERE userId = ?"
+    ).get(_userId) as IUserInfo|undefined;
+    console.log("fetchUser :: 結果->", userInfo);
+
+    if (userInfo !== undefined) {
+      resolve(userInfo);
+      return;
+    }
+
+    resolve(null);
+    return;
   });
 }
