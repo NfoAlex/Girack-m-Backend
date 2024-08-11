50135a303d1e2cf30e10ccbb8ed6f2395b2511f9
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-12 05:05:12 +0900

    [change] - fetchSessionをbetter-sqlite3へ

--- src/actionHandler/auth/fetchSession.ts
+++ src/actionHandler/auth/fetchSession.ts
@@ -1,6 +1,10 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./records/USER.db");
 
+import Database from 'better-sqlite3';
+const _db = new Database('./records/USER.db', {verbose: console.log });
+_db.pragma('journal_mode = WAL');
+
 import type { IUserSession } from "../../type/User";
 
 /**
@@ -9,30 +13,18 @@ import type { IUserSession } from "../../type/User";
  * @param _indexNumber 
  * @returns 
  */
-export default async function fetchSession(_userId: string, _indexNumber: number)
-:Promise<IUserSession[] | null> {
+export default function fetchSession(_userId: string, _indexNumber: number)
+:IUserSession[] | null {
   try {
 
     //オフセットでずらすデータ数
     const offsetNum = 10 * (_indexNumber-1 || 0);
 
-    return new Promise((resolve) => {
-      db.all(
-        "SELECT * FROM USERS_SESSION WHERE userId=? LIMIT 10 OFFSET ?",
-        [_userId, offsetNum],
-        (err:Error, sessionData:IUserSession[]) => {
-          //エラーハンドラ
-          if (err) {
-            resolve(null);
-            return;
-          }
+    const sessionData = _db.prepare(
+      "SELECT * FROM USERS_SESSION WHERE userId=? LIMIT 10 OFFSET ?"
+    ).all(_userId, offsetNum) as IUserSession[];
 
-          //データを返す
-          resolve(sessionData);
-          return;
-        }
-      );
-    });
+    return sessionData;
 
   } catch(e) {
 
