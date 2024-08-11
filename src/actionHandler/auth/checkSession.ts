09bda75fd51015416622f14ca226c4443dde8be8
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-12 04:18:35 +0900

    [change] - checkSessionをbetter-sqlite3へ

--- src/actionHandler/auth/checkSession.ts
+++ src/actionHandler/auth/checkSession.ts
@@ -1,69 +1,49 @@
-import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./records/USER.db");
 import fetchUser from "../User/fetchUser";
 import type { IUserSession } from "../../type/User";
 import type IRequestSender from "../../type/requestSender";
 
+import Database from 'better-sqlite3';
+const db = new Database('./records/USER.db', {verbose: console.log });
+db.pragma('journal_mode = WAL');
+
 export default async function checkSession(RequestSender:IRequestSender)
 :Promise<boolean> {
-  return new Promise<boolean> (async (resolve) => {
-    try {
+  try {
 
-      //データ確認
-      if (RequestSender.userId === undefined && RequestSender.sessionId === undefined) {
-        resolve(false);
-        return;
-      }
-
-      //ユーザー情報があるか、BANされているかどうかを確認
-      const userInfo = await fetchUser(RequestSender.userId, null);
-      if (userInfo === null) {
-        resolve(false);
-        return;
-      }
-      if (userInfo.banned) {
-        resolve(false);
-        return;
-      }
-
-      //ユーザーIdで検索、セッションIDを一致を探す
-      db.all(
-        "SELECT * FROM USERS_SESSION WHERE userId = ?",
-        [RequestSender.userId],
-        (err:Error, datSession:IUserSession[]
-      ) => {
-        if (err) {
-          console.log("checkSession :: db : ERROR ->", err);
-          resolve(false);
-          return;
-        } else {
-          //console.log("checkSession :: 検索結果->", datSession);
-          //データが空なら終わらせる
-          if (datSession.length === 0) {
-            resolve(false);
-            return;
-          }
-
-          //セッションデータ分ループしてセッションIDの一致を探す
-          for (let dat of datSession) {
-            if (dat.sessionId === RequestSender.sessionId) {
-              resolve(true);
-              return;
-            }
-          }
-
-          //ループ抜けちゃったら失敗として返す
-          resolve(false);
-          return;
-        }
-      });
-
-    } catch(e) {
-
-      console.log("checkSesion :: エラー->", e);
-      resolve(false);
-      return;
-
+    //データ確認
+    if (RequestSender.userId === undefined && RequestSender.sessionId === undefined) {
+      return false;
     }
-  });
+
+    //ユーザー情報があるか、BANされているかどうかを確認
+    const userInfo = await fetchUser(RequestSender.userId, null);
+    if (userInfo === null) {
+      return false;
+    }
+    if (userInfo.banned) {
+      return false;
+    }
+
+    //セッションデータ取得用の
+    const stmtSessionData = db.prepare(
+      "SELECT * FROM USERS_SESSION WHERE userId = ?"
+    );
+    //ループ用データ取得処理部分
+    const iterateSessionData = stmtSessionData.iterate(RequestSender.userId) as Iterable<IUserSession>;
+
+    //ループして一致するセッションデータを探す
+    for (const session of iterateSessionData) {
+      if (session.sessionId === RequestSender.sessionId) {
+        return true;
+      }
+    }
+
+    return false;
+
+  } catch(e) {
+
+    console.log("checkSesion :: エラー->", e);
+    return false;
+
+  }
 }
