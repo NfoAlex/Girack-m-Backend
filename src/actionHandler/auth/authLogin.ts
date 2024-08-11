5bef298ccc3c20ea32be3122aa0e0167098bb5fa
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-12 03:26:28 +0900

    [change] - authLoginをbetter-sqlite3化

--- src/actionHandler/auth/authLogin.ts
+++ src/actionHandler/auth/authLogin.ts
@@ -1,8 +1,10 @@
-import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./records/USER.db");
 import fetchUser from "../User/fetchUser";
 
-import { IUserInfo, IUserPassword } from "../../type/User";
+import Database from 'better-sqlite3';
+const db = new Database('./records/USER.db', {verbose: console.log });
+db.pragma('journal_mode = WAL');
+
+import type { IUserInfo, IUserPassword } from "../../type/User";
 
 export default async function authLogin(username:string, password:string)
 :Promise<{authResult:boolean, UserInfo:IUserInfo|null, sessionId:string|null}> {
@@ -16,23 +18,21 @@ export default async function authLogin(username:string, password:string)
     if (RESULT === null) return {authResult:false, UserInfo:null, sessionId:null};
 
     //パスワードを比較して結果保存
-    const authResult:boolean = await new Promise(async (resolve) => {
-      db.all("SELECT * FROM USERS_PASSWORD WHERE userId = ?", [RESULT.userId], (err:Error, datUser:IUserPassword[]) => {
-        if (err) {
-          console.log("authLogin :: authLogin(db) : ERROR ->", err);
-          resolve(false);
-        } else {
-          //console.log("authLogin :: authLogin(db) : 検索結果->", datUser);
-          //パスワードが合っているならtrueに
-          if (datUser[0].password === password) {
-            //console.log("authLogin :: authLogin(db) : パスワード合ってるね")
-            resolve(true);
-          } else {
-            //console.log("authLogin :: authLogin(db) : パスワード違う");
-            resolve(false);
-          }
+    const authResult:boolean = await new Promise((resolve) => {
+      const passwordData =
+        db.prepare("SELECT * FROM USERS_PASSWORD WHERE userId=?")
+        .get(RESULT.userId) as IUserPassword|null;
+      
+      console.log("authLogin :: passwordData->", passwordData);
+      if (passwordData !== null) {
+        if (passwordData.password === password) {
+          resolve(true);
+          return;
         }
-      });
+      }
+
+      resolve(false);
+      return;
     });
 
     //違うなら失敗結果を返す
@@ -40,11 +40,9 @@ export default async function authLogin(username:string, password:string)
 
     //セッション情報を作成してDBへ挿入
     const sessionIdGen = generateSessionId();
-    db.run("insert into USERS_SESSION(userId, sessionId, sessionName) values(?,?,?)",
-      RESULT.userId,
-      sessionIdGen,
-      "ログイン"
-    );
+    db.prepare(
+      "INSERT INTO USERS_SESSION(userId, sessionId, sessionName) values(?,?,?)"
+    ).run(RESULT.userId, sessionIdGen, "ログイン");
     
     return {authResult:true, UserInfo:RESULT, sessionId:sessionIdGen};
 
