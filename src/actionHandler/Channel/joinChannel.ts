b3685f56b3f314654833e7130afbe5d92da16248
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-12 15:09:20 +0900

    [change] - joinChannelをbetter-sqlite3へ

--- src/actionHandler/Channel/joinChannel.ts
+++ src/actionHandler/Channel/joinChannel.ts
@@ -3,63 +3,52 @@ const db = new sqlite3.Database("./records/USER.db");
 import fetchUser from "../User/fetchUser";
 import fetchChannel from "./fetchChannel";
 
-export default async function joinChannel(userId:string, channelId:string)
+import Database from 'better-sqlite3';
+const _db = new Database('./records/USER.db');
+_db.pragma('journal_mode = WAL');
+
+/**
+ * チャンネルへ参加させる
+ * @param _userId 
+ * @param _channelId 
+ * @returns 
+ */
+export default async function joinChannel(_userId:string, _channelId:string)
 :Promise<boolean> {
   try {
-    
-    return new Promise(async (resolve) => {
-      //チャンネルの存在を確認しないなら停止
-      if (await fetchChannel(channelId, userId) === null) {
-        resolve(false);
-        return;
-      }
-      
-      //TODO :: ロール確認(ChannelViewPrivate)
 
-      //現在のユーザー情報を取得
-      const userInfo = await fetchUser(userId, null);
-      //情報が空なら処理停止
-      if (userInfo === null) {
-        resolve(false);
-        return;
-      }
-      //参加しているチャンネルの配列抜き出し
-      const channelJoinedArr = userInfo.channelJoined;
-      
-      //参加チャンネル配列にすでにチャンネルIDが入ってるなら停止
-      if (channelJoinedArr.includes(channelId)) {
-        resolve(false);
-        return;
-      }
+    //チャンネルの存在を確認しないなら停止
+    if (await fetchChannel(_channelId, _userId) === null) {
+      return false;
+    }
 
-      //参加配列が空([""])なら代入、違うならチャンネルIDを配列へ追加
-      if (channelJoinedArr.length === 1 && channelJoinedArr[0] === "") {
-        channelJoinedArr[0] = channelId;
-      } else {
-        channelJoinedArr.push(channelId);
-      }
+    //現在のユーザー情報を取得
+    const userInfo = await fetchUser(_userId, null);
+    //情報が空なら処理停止
+    if (userInfo === null) {
+      return false;
+    }
+    //参加しているチャンネルの配列抜き出し
+    const channelJoinedArr = userInfo.channelJoined;
 
-      //DBにて更新
-      db.run(
-        "UPDATE USERS_INFO SET channelJoined=? WHERE userId=?",
-        [
-          channelJoinedArr.join(","), //更新した参加チャンネル配列
-          userId //参加するユーザーID
-        ],
-        (err) => {
-          if (err) {
-            console.log("joinChannel :: db : エラー->", err);
-            //エラーなら失敗
-            resolve(false);
-            return;
-          } else {
-            //無事なら成功
-            resolve(true);
-            return;
-          }
-        }
-      );
-    });
+    //参加チャンネル配列にすでにチャンネルIDが入ってるなら停止
+    if (channelJoinedArr.includes(_channelId)) {
+      return false;
+    }
+
+    //参加配列が空([""])なら代入、違うならチャンネルIDを配列へ追加
+    if (channelJoinedArr.length === 1 && channelJoinedArr[0] === "") {
+      channelJoinedArr[0] = _channelId;
+    } else {
+      channelJoinedArr.push(_channelId);
+    }
+
+    //ユーザーのレコードへ挿入
+    _db.prepare(
+      "UPDATE USERS_INFO SET channelJoined=? WHERE userId=?"
+    ).run(channelJoinedArr.join(","), _userId);
+
+    return true;
 
   } catch(e) {
 
