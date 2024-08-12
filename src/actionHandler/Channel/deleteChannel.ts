a9d3ed28e8771f5abd89a5c6a49fada3bc681bd8
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-12 14:21:05 +0900

    [change] - deleteChannelをbetter-sqlite3へ

--- src/actionHandler/Channel/deleteChannel.ts
+++ src/actionHandler/Channel/deleteChannel.ts
@@ -1,46 +1,40 @@
-import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./records/SERVER.db");
 import fetchChannel from "./fetchChannel";
 import roleCheck from "../../util/roleCheck";
 
+import Database from 'better-sqlite3';
+const db = new Database('./records/SERVER.db');
+db.pragma('journal_mode = WAL');
+
+/**
+ * チャンネルを削除する
+ * @param _userId 削除する操作元のユーザーId
+ * @param _channelId 削除するチャンネルId
+ * @returns 
+ */
 export default async function deleteChannel(
-    userId: string,
-    channelId: string
+    _userId: string,
+    _channelId: string
   ):Promise<boolean> {
   try {
 
     //チャンネル情報を取得
-    const channelInfo = await fetchChannel(channelId, userId);
+    const channelInfo = await fetchChannel(_channelId, _userId);
     //無ければ停止
     if (channelInfo === null) return false;
 
     //チャンネル作成者でない場合権限があるかを確認
-    if (channelInfo.createdBy !== userId) {
+    if (channelInfo.createdBy !== _userId) {
       //チャンネル管理のロール権限を確認する
-      const roleCheckResult = await roleCheck(userId, "ChannelManage");
+      const roleCheckResult = await roleCheck(_userId, "ChannelManage");
       if (!roleCheckResult) { //falseなら停止
         return false;
       }
     }
 
-    return new Promise((resolve) => {
-      //テーブルからチャンネルを削除
-      db.run(
-        "DELETE FROM CHANNELS WHERE channelId=?",
-        channelId,
-        (err) => {
-          //結果に応じてbooleanで結果を返す
-          if (err) {
-            resolve(false);
-            return;
-          } else {
-            resolve(true);
-            return;
-          }
-        }
-      );
-      
-    });
+    //チャンネルのテーブルを削除
+    db.prepare("DELETE FROM CHANNELS WHERE channelId=?").run(_channelId);
+
+    return true;
 
   } catch(e) {
 
