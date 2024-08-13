6f37d0349feeea3eb920bb9d69fbb423d7907578
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-13 14:24:41 +0900

    [change] - 変数名とコメント

--- src/actionHandler/File/calcFullFolderSize.ts
+++ src/actionHandler/File/calcFullFolderSize.ts
@@ -1,15 +1,17 @@
 import calcDirectorySize from "../../util/FIle/calcDirectorySize";
 
 /**
- * フォルダのサイズを取得
+ * 指定のユーザーId用フォルダの総サイズを取得
+ * @param _userId 
+ * @returns 
  */
 export default async function calcFullFolderSize(
-  userId: string,
+  _userId: string,
 ):Promise<number|null> {
   try {
 
     //容量を計算する、できなかったらエラー
-    const folderSize = await calcDirectorySize(userId, "");
+    const folderSize = await calcDirectorySize(_userId, "");
     if (folderSize === null) {
       return null;
     }
