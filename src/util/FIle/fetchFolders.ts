e3e4ae2b17d0af48d54cc6275eb0078f8bae666a
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-13 14:21:47 +0900

    [change] - fetchFoldersをbetter-sqlite3へ

--- src/util/FIle/fetchFolders.ts
+++ src/util/FIle/fetchFolders.ts
@@ -1,33 +1,32 @@
-import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./records/FILEINDEX.db");
+import Database from 'better-sqlite3';
+const db = new Database('./records/FILEINDEX.db');
+db.pragma('journal_mode = WAL');
+
 import type { IFolder } from "../../type/File";
 
+/**
+ * 指定のフォルダに属するフォルダId群を取得する
+ * @param _userId 
+ * @param _positionedDirectoryId 
+ * @returns 
+ */
 export default async function fetchFolders(
-  userId: string,
-  positionedDirectoryId: string = ""
+  _userId: string,
+  _positionedDirectoryId = ""
 ):Promise<IFolder[]|null> {
   try {
-    
-    return new Promise((resolve) => {
-      db.all(
-        `
-        SELECT * FROM FOLDERS
-          WHERE userId=? AND positionedDirectoryId=?
-        `,
-        [userId, positionedDirectoryId],
-        (err:Error, fileInfo:IFolder[]) => {
-          if (err) {
-            console.log("fetchFolders :: db : エラー->", err);
-            resolve(null);
-            return;
-          } else {
-            console.log("fetchFolders :: db : 結果->", fileInfo);
-            resolve(fileInfo);
-            return;
-          }
-        }
-      );
-    });
+
+    //フォルダId群を取得する
+    const folders = db.prepare(
+      `
+      SELECT * FROM FOLDERS
+        WHERE userId=? AND positionedDirectoryId=?
+      `
+    ).all(_userId, _positionedDirectoryId) as IFolder[] | undefined;
+    //フォルダ情報がundefinedならnull
+    if (folders === undefined) return null;
+
+    return folders;
 
   } catch(e) {
 
