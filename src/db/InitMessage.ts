8f9054d411458ba53f0b92a9649ec01000790e81
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-08-14 23:06:37 +0900

    [change] - InitMessageをbetter-sqlite3へ

--- src/db/InitMessage.ts
+++ src/db/InitMessage.ts
@@ -1,18 +1,18 @@
-import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./records/MESSAGE.db");
+import Database from 'better-sqlite3';
+const db = new Database('./records/MESSAGE.db');
+db.pragma('journal_mode = WAL');
 
-import migration20240603 from "./migration/Message/20240603";
+import migrationMessage20240603 from "./migration/Message/20240603";
 import migrationMessage20240709 from "./migration/Message/20240709";
 import migrationMessage20240714 from "./migration/Message/20240714";
 
-db.serialize(() => {
-  //migration
-  migration20240603();
-  migrationMessage20240709();
-  migrationMessage20240714();
+//migration
+migrationMessage20240603();
+migrationMessage20240709();
+migrationMessage20240714();
 
-  //randomチャンネル用のテーブル作成
-  db.run(
+//randomチャンネル用のテーブル作成
+db.exec(
   `create table if not exists C0001(
     messageId TEXT PRIMARY KEY,
     channelId TEXT NOT NULL,
@@ -23,9 +23,9 @@ db.serialize(() => {
     fileId TEXT NOT NULL,
     time TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     reaction TEXT NOT NULL
-  )`);
+  )`
+);
 
-  console.log("InitMessage :: db : メッセージ用DB作成完了");
-});
+console.log("InitMessage :: db : メッセージ用DB作成完了");
 
 db.close();
