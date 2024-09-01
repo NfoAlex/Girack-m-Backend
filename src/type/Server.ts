73016464052a62355930aa67045b7598dc9ceed1
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-01 10:11:29 +0900

    [change] - Interfaceを書くところ移動

--- src/type/Server.ts
+++ src/type/Server.ts
@@ -25,3 +25,21 @@ export default interface IServerInfo {
     }
   }
 };
+
+export interface IAPIClientInfo {
+  apiClientId: string,
+  clientName: string,
+  description: string,
+  createBy: string,
+  status: boolean,
+  approvedStatus: "WAITING" | "APPROVED" | "BANNED"
+}
+
+export interface IAPIClientInfoBeforeParsing {
+  apiClientId: string,
+  clientName: string,
+  description: string,
+  createBy: string,
+  status: 1 | 0,
+  approvedStatus: "WAITING" | "APPROVED" | "BANNED"
+}
